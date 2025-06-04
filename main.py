922637095a5c4bce2c21497d209735dc370a5b5e
princzp <peter.princz@idomsoft.hu>
2025-06-04 13:59:15 +0200

    fixed main.py

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
-import json
 import logging as log
-
-
-from ai.commentAnalyzer import CommentAnalyzer
-
 
 if __name__ == "__main__":
     log.basicConfig(
         level=log.INFO,
         format="%(asctime)s [%(levelname)s] %(name)s: %(message)s"
     )
-    config_path = "config.json"
-    configuration = None
-    try:
-        with open(config_path, "r", encoding="utf-8") as f:
-            configuration = json.load(f)
-    except Exception as e:
-        log.error(f"Error loading config file '{config_path}': {e}")
-        raise e
-    analyzer = CommentAnalyzer(configuration)
-    result = analyzer.analyze_comment("I'm really bullish on AMD right now — their earnings blew expectations!")
-    log.info(result)
+    log.info("Hello world!")
