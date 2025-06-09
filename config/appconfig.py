b6d233bd36dec3dc742cf779965f0abc19b7cff3
Kevin <gergelykevin123@gmail.com>
2025-06-09 18:14:49 +0200

    feat: added functionality to read new config

--- config/appconfig.py
+++ config/appconfig.py
@@ -2,24 +2,40 @@ from pydantic import BaseModel
 import json
 import os
 
+
 class AppConfig(BaseModel):
-    ollama_url:str
-    model:str
-    prompt_path:str
-    client_id:str
-    client_secret:str
-    password:str
-    user_agent:str
-    username:str
-    database_uri:str
+    ollama_url: str
+    model: str
+    prompt_path: str
+    client_id: str
+    client_secret: str
+    password: str
+    user_agent: str
+    username: str
+    database_uri: str
+
+
+class TradingConfig(BaseModel):
+    key: str
+    secret: str
+
 
 def load_config(path: str = "config/config.json") -> AppConfig:
     newpath = (str)(os.path.dirname(__file__)) + "\config.json"
-    print(newpath)
     with open(newpath, "r") as f:
         data = json.load(f)
     appconfig = AppConfig(**data)
     appconfig.password = os.getenv("reddit_password", "secret")
     return appconfig
 
+
+def load_trading_config() -> TradingConfig:
+    path = (str)(os.path.dirname(__file__)) + "\\tradingconfig.json"
+    with open(path, "r") as f:
+        data = json.load(f)
+    trading_config = TradingConfig(**data)
+    return trading_config
+
+
 config: AppConfig = load_config()
+tradingConfig: TradingConfig = load_trading_config()
